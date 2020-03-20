7aaa6f034151950111a44d3c2271c23ed10b74ee
SouaadMa <51034305+SouaadMa@users.noreply.github.com>
2020-03-20 19:38:23 +0100

    Initial commit

---
+++ ProjectTrial2/Etudiant.vb
@@ -0,0 +1,23 @@
+﻿Public Class Etudiant
+    Implements IComparable(Of Etudiant)
+
+    Private Id As String
+
+    Public Function getId() As String
+        Return Me.Id
+    End Function
+
+    Public Function CompareTo(ByVal other As Etudiant) As Integer _
+        Implements IComparable(Of WindowsApplication1.Etudiant).CompareTo
+
+        Return (Me.Id.CompareTo(other.getId()))
+
+    End Function
+
+    Public Function getInfo(ByVal champs As String) As Object
+        'Return ClasseBDD.getInfoBDD(Id, champs)
+        Return Nothing
+
+    End Function
+
+End Class

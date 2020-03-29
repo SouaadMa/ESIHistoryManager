d3a7b0518989e121226184f7209e3cf4fe10d71a
SouaadMa <51034305+SouaadMa@users.noreply.github.com>
2020-03-29 21:38:51 +0100

    Added Classement

---
+++ ProjectTrial2/Classement.vb
@@ -0,0 +1,46 @@
+﻿Public Class Classement
+    
+
+    Public Shared Function SortCollection(ByVal collection As List(Of Etudiant), ByVal champs As String) As List(Of Etudiant)
+
+        ' Création du comparateur selon le champs donné
+        Dim cust_comparer As New  _
+            ComparateurEtudiant(champs)
+
+        ' Le tri
+        collection.Sort(cust_comparer)
+
+        Return collection
+    End Function
+
+
+    'Classe interne qui aide avec la comparaison avec plusieurs champs
+    Private Class ComparateurEtudiant
+        Implements IComparer(Of Etudiant)
+
+        ' L'attribut décide le champs avec lequel les étudiants seront comparés.
+        Public ChampsComparaison As String = "Matricule"
+
+        Public Sub New(ByVal compare_type As String)
+            ChampsComparaison = compare_type
+        End Sub
+
+        ' Méthode qui compare deux étudiants selon l'attribut ChampsComparaison de la classe.
+        Public Function Compare(ByVal x As Etudiant, ByVal y As  _
+            Etudiant) As Integer Implements _
+            System.Collections.Generic.IComparer(Of Etudiant).Compare
+
+            'On fait le cast des deux objets en entrée
+            Dim x_cust As Etudiant = DirectCast(x,  _
+                Etudiant)
+            Dim y_cust As Etudiant = DirectCast(y,  _
+                Etudiant)
+
+            Return x_cust.getInfo(ChampsComparaison).CompareTo(y_cust.getInfo(ChampsComparaison))
+
+        End Function
+    End Class
+
+
+End Class
+

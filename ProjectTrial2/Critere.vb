b1117b95ccd980c0132db3fc643234239ec3505f
SouaadMa <51034305+SouaadMa@users.noreply.github.com>
2020-04-02 01:24:00 +0100

    Prepare for Modify
    Update Critere, and added table names to BDD

--- ProjectTrial2/Critere.vb
+++ ProjectTrial2/Critere.vb
@@ -1,10 +1,17 @@
 ﻿Public Class Critere
     Private champs As String 'Le nom du champs
     Private valeur As Object 'La valeur correspondante au champs
+    Private table As String 'Le nom de la table qui contient le champs
 
     Public Sub New(ByVal cham As String, ByVal val As Object) 'Constructeur
         champs = cham
         valeur = val
+    End Sub
+
+    Public Sub New(ByVal cham As String, ByVal val As Object, ByVal tab As String) 'Deuxième constructeur (pour modifier)
+        champs = cham
+        valeur = val
+        table = tab
     End Sub
 
     Public Function getChamps() As String 'Getter pour le champs
@@ -13,6 +20,10 @@
 
     Public Function getValeur() As Object 'Getter pour la valeur
         Return valeur
+    End Function
+
+    Public Function getTable() As String 'Getter pour le nom de la table
+        Return table
     End Function
 
 End Class

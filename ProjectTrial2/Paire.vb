1a918586ad515ae17a02324a54fe14d3302400f5
ANFAL23 <58882571+ANFAL23@users.noreply.github.com>
2020-04-14 16:59:40 +0100

    Create Paire.vb
    Paire

---
+++ ProjectTrial2/Paire.vb
@@ -0,0 +1,25 @@
+﻿Public Class Paire
+
+    Public elem1 As String
+    Public elem2 As String
+
+    Public Sub New(ByVal ele1 As String, ByVal ele2 As String)
+        elem1 = ele1
+        elem2 = ele2
+    End Sub
+
+    Public Function getElement(ByVal ind As Integer)
+        Select Case ind
+            Case 1
+                Return elem1
+            Case 2
+                Return elem2
+            Case Else
+                Return ""
+        End Select
+    End Function
+
+End Class
+
+
+

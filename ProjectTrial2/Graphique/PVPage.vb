f6f69eb70d3e7705d2eaa00e8d8e91ede15444ab
HafsaKaraAchira <59230432+HafsaKaraAchira@users.noreply.github.com>
2020-04-25 01:42:03 +0100

    adding some comments

--- ProjectTrial2/Graphique/PVPage.vb
+++ ProjectTrial2/Graphique/PVPage.vb
@@ -8,6 +8,8 @@ Imports System.Data.SqlClient
 Public Class PVPage
 
     Private Sub PVPage_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'initialize the graphic componants values
+
         CrystalReportViewer1.Enabled = False
         Me.AffichButton.Enabled = Not Me.AffichButton.Enabled
 
@@ -29,15 +31,15 @@ Public Class PVPage
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AffichButton.Click
+
         CrystalReportViewer1.Enabled = True
-
-        Dim ds As New DataSet
-        Dim dt As New DataTable()
-
+        Dim ds As New DataSet   'the dataset which will be assigned to the report
         Dim cryrpt As New PvReport
 
-        ds = SortiePV.PV_DELIBERATION(NiveauBox.Text.Remove(0, 1), SpecialiteBox.Text, PromoBox.Text.Remove(0, 2))
+        'call the pv function to get the dataset
 
+        ds = SortiePV.PV_DELIBERATION(NiveauBox.Text.Remove(0, 1), SpecialiteBox.Text, PromoBox.Text.Remove(0, 2))
+        ' if there is a limit to viewed students , then remove the rest from the datatable
         If Not AllCheckBox.Checked Then
             'FormulaField = FormulaFields.Item("limit")
             'FormulaField.Text = LimitUpDown.Value.ToString + " ;"
@@ -46,6 +48,7 @@ Public Class PVPage
             End While
         End If
 
+        'initilize the textboxes of promotion and annee
         Dim TextObj As CrystalDecisions.CrystalReports.Engine.TextObject
 
         For Each CryObj As CrystalDecisions.CrystalReports.Engine.ReportObject In cryrpt.Section2.ReportObjects
@@ -60,42 +63,50 @@ Public Class PVPage
             End If
         Next
 
+        'assign the dataset first table to the report
+
         cryrpt.SetDataSource(ds.Tables(0))
         cryrpt.Database.Tables(0).SetDataSource(ds)
 
-        Dim FormulaFields As FormulaFieldDefinitions
-        Dim FormulaField As FormulaFieldDefinition
+        'get the collection of formulas fields in the report
+
+        Dim FormulaFields As FormulaFieldDefinitions    ' the collection 
+        Dim FormulaField As FormulaFieldDefinition      ' an item of the collection
 
         FormulaFields = cryrpt.DataDefinition.FormulaFields
 
-        For i As Integer = 9 To ds.Tables(0).Columns.Count - 1 Step 1
+        For i As Integer = 9 To ds.Tables(0).Columns.Count - 1 Step 1   ' for each subject in the datatable
+            'get the corresponding formula item
             FormulaField = FormulaFields.Item("UnboundNumber" + (i - 8).ToString)
+            'assign the corresponding subject colonnes to the formula
             FormulaField.Text = "{" + cryrpt.Database.Tables.Item(0).Name + "." + cryrpt.Database.Tables(0).Fields.Item(i).Name + "} ;"
+            ' get the corresponding column header  
             TextObj = CType(cryrpt.Section2.ReportObjects.Item("UnboundNumber" + (i - 8).ToString + "1Text"), CrystalDecisions.CrystalReports.Engine.TextObject)
+            ' assign it the subject name and coeff
             TextObj.Text = ds.Tables.Item(1).Rows(i - 9)(BDD.champsCOMAMA).ToString + Environment.NewLine + ds.Tables.Item(1).Rows(i - 9)(BDD.champsCOEFMA).ToString
         Next
 
-        'Dim FieldObj As CrystalDecisions.CrystalReports.Engine.FieldObject
-
-        
-
+        ' calculate the blank space in the report (if the subjects counts is less than 8 )
         Dim blankSpace As Integer = 8 - ds.Tables(1).Rows.Count
-        If 0 < blankSpace Then
 
+        'if so
+        If 0 < blankSpace Then
+            'get the report fiels of en tete section and details section
             Dim Fields2 As CrystalDecisions.CrystalReports.Engine.ReportObjects = cryrpt.Section2.ReportObjects
             Dim Fields3 As CrystalDecisions.CrystalReports.Engine.ReportObjects = cryrpt.Section3.ReportObjects
             Dim Fieldsnames As New List(Of String)
 
+            ' get the details fields names
             For Each field As FieldObject In Fields3
-                'If Not field.Name.Contains("UnboundNumber") Then
                 Fieldsnames.Add(field.Name)
-                'End If
             Next
+            'the blank space calculation
             blankSpace *= Fields3.Item("UnboundNumber11Text").Width
-
+            ' assign its values to the blankspace field formula
             FormulaField = FormulaFields.Item("blankSpace")
             FormulaField.Text = blankSpace.ToString + ";"
 
+            'remove the names of the suppressed columns from the report
             For i As Integer = 8 To ds.Tables(1).Rows.Count + 1 Step -1
                 TextObj = Fields2.Item("UnboundNumber" + i.ToString + "1Text")
                 Fieldsnames.Remove("UnboundNumber" + i.ToString + "1Text")
@@ -105,12 +116,16 @@ Public Class PVPage
                 TextObj.Height = 0
             Next
 
-            While Fieldsnames.Count <> 0
+            While Fieldsnames.Count <> 0    'for each field in thed etails section
+                'if the field at the right of blank space , then shift it with its header to the right and suppress their names 
                 If (Fields3.Item(Fieldsnames.Item(0)).Left >= cryrpt.Section2.ReportObjects.Item("ELIMININ1").Left) Then
                     Fields3.Item(Fieldsnames.Item(0)).Left -= blankSpace \ 2
                     Fields2.Item(Fieldsnames.Item(0) + "Text").Left -= blankSpace \ 2
                     Fieldsnames.Remove(Fieldsnames.Item(0))
-                Else
+                Else    ' if it is on the right , then shift it to the right with its header , but if it is the column of a formula , them
+                    ' then shift just the header (crytal don't move formula fileds without a formula , the formula is in crytal
+                    ' it maniplate the blankspace formula filed that was sent earlier to the report 
+                    ' and then suppress their names
                     If (Not Fieldsnames.Item(0).Contains("UnboundNumber")) Then
                         Fields3.Item(Fieldsnames.Item(0)).Left += blankSpace \ 2
                     End If
@@ -120,16 +135,21 @@ Public Class PVPage
             End While
 
         End If
-
-            CrystalReportViewer1.RefreshReport()
+        ' refresh the report to view the changes
+        CrystalReportViewer1.RefreshReport()
+        'Set the report viewer report objet 
             CrystalReportViewer1.ReportSource = cryrpt
-            CrystalReportViewer1.Zoom(1)
+        'set the zoom factor the width of the page
+        CrystalReportViewer1.Zoom(1)
+        ' if the bule side bar is opened , then close it by simulating a mouse click on the menu button to make the view more big
             If (Home.MainContainer1.Width <= 700) Then
             Home.MenuButton_Click(Home.MenuButton, New MouseEventArgs(MouseButtons.Middle, 1, 15, 15, 0))
-            End If
-            CrystalReportViewer1.Visible = True
+        End If
+        ' make the report viewer visible
+        CrystalReportViewer1.Visible = True
+        'disbale the affich button until any combobox values has been changed
         AffichButton.Enabled = False
-
+        ' and that's it
     End Sub
 
     Private Sub PromoBox_TextUpdate(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SpecialiteBox.TextUpdate, PromoBox.TextUpdate, NiveauBox.TextUpdate
